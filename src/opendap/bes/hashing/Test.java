7d0eda1a97c497d1de02366242b823b7410c8718
Niklas Griessbaum <griessbaum@gmail.com>
2018-10-19 12:54:19 -0700

    hash

---
+++ src/opendap/bes/hashing/Test.java
@@ -0,0 +1,11 @@
+package opendap.bes.hashing;
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        HashLog hashLog = new HashLog();
+        hashLog.connect();
+    }
+
+}
